bf63df323b114ecebf41378951cb3ddbb2a13287
Saumz <saumyajeetbhowmick@gmail.com>
2026-02-24 01:41:20 +0530

    Refactor chat validation middleware and update chat routes to include validation checks

--- src/middleware/chatValidation.middleware.js
+++ src/middleware/chatValidation.middleware.js
@@ -2,7 +2,16 @@ import { APIError } from "../utils/APIError.js";
 import { chatValidation } from "../validations/chat.validation.js";
 
 
-export const userValidate = (req, res, next) => {
+export const chatValidate = (req, res, next) => {
     const errors = chatValidation(req)
-    if (!errors.isEm) return
+    if (errors.isEmpty()) return next()
+
+    const extractedErrors = []
+    errors.array().map((error) => extractedErrors.push(
+        {
+            [error.path]: error.msg
+        }
+    ))
+
+    throw new APIError(423, "Credentials passed are not valid:", extractedErrors)
 }
--- src/routes/chat.route.js
+++ src/routes/chat.route.js
@@ -2,12 +2,14 @@ import { Router } from "express";
 import { verifyJwt } from "../middleware/auth.middleware.js";
 import { upload } from "../middleware/multer.middleware.js";
 import { chat } from "../controller/chat.controller.js";
+import { chatSendValidation } from "../validations/chat.validation.js";
+import { chatValidate } from "../middleware/chatValidation.middleware.js";
 
 const router = Router()
 
 router.use(verifyJwt)
 
-router.route("/").get(upload.none(), chat)
-router.route("/c/:conversationId").get(upload.none(), chat)
+router.route("/").get(upload.none(), chatSendValidation(), chatValidate, chat)
+router.route("/c/:conversationId").get(upload.none(), chatSendValidation(), chatValidate, chat)
 
 export { router as chatRouter }
